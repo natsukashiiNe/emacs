d8a655dc60fd5a95af1606b94bf47d71e93e6850
Alexander Vasilyev <natsukashii_nane@proton.me>
2025-09-03 06:58:01 +0300

    Refactored LSP config

--- lsp/lsp-config.el
+++ lsp/lsp-config.el
@@ -1,16 +1,25 @@
+;;; lsp-config.el --- LSP Mode Configuration -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Unified LSP configuration for all languages
+
+;;; Code:
 (use-package lsp-mode
   :ensure t
   :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "C-c l"
-        lsp-auto-configure t
-        lsp-enable-snippet nil
-        lsp-session-file "~/.emacs.d/lsp-session-v1"
-        lsp-prefer-flymake nil
-        lsp-idle-delay 0.2
-        lsp-log-io nil
-        lsp-enable-symbol-highlighting t
-        lsp-semantic-tokens-apply-modifiers t)
+  :custom
+  (lsp-keymap-prefix "C-c l")
+  (lsp-auto-configure t)
+  (lsp-enable-snippet nil)
+  (lsp-session-file "~/.emacs.d/lsp-session-v1")
+  (lsp-prefer-flymake nil)
+  (lsp-idle-delay 0.2)
+  (lsp-log-io nil)
+  (lsp-enable-symbol-highlighting t)
+  (lsp-semantic-tokens-apply-modifiers t)
+  (lsp-auto-guess-root t)
+  (xref-search-program 'ripgrep)
+  (xref-backend-functions '(lsp--xref-backend))
 
   :hook ((c-mode . lsp)
          (c++-mode . lsp)
@@ -18,13 +27,8 @@
          (python-mode . lsp)
          (prog-mode . (lambda ()
                         (unless (derived-mode-p 'emacs-lisp-mode)
-                          (lsp)))))  ;; Disable LSP for Emacs Lisp
+                          (lsp))))))
 
-  ;;:hook ((c-mode  . lsp-deferred)
-  ;;       (c++-mode . lsp-deferred))
 
-  :config
-  (setq lsp-auto-guess-root t)
-  (setq xref-search-program 'ripgrep)
-  (setq xref-backend-functions '(lsp--xref-backend)))
-
+(provide 'lsp-config)
+;;; lsp-config.el ends here
--- lsp/lsp-servers.el
+++ lsp/lsp-servers.el
@@ -1,19 +1,32 @@
-(setq lsp-clients-clangd-executable "/usr/bin/clangd"
-      lsp-clients-clangd-args
-      '("--background-index"
-        "--pch-storage=memory"
-        "--clang-tidy"
-        "--suggest-missing-includes"
-        "--header-insertion=iwyu"
-        "--index-file=global"
-        "--compile-commands-dir=build"))
+;;; lsp-servers.el --- LSP Mode Configuration -*- lexical-binding: t; -*-
 
+;;; Commentary:
+;; Setting up different LSP servers
+
+;;; Code:
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :custom
+  ;; clangd client options belong to lsp-mode (they're defcustoms there)
+  (lsp-clients-clangd-executable "/usr/bin/clangd")
+  (lsp-clients-clangd-args
+   '("--background-index"
+     "--pch-storage=memory"
+     "--clang-tidy"
+     "--suggest-missing-includes"
+     "--header-insertion=iwyu"
+     "--index-file=global"
+     "--compile-commands-dir=build")))
+
+;; Associate C++ modules with tree-sitter mode
 (add-to-list 'auto-mode-alist '("\\.cppm\\'" . c++-ts-mode))
+
 (use-package flycheck-clang-tidy
-  :ensure t
   :after flycheck
+  :commands (flycheck-clang-tidy-setup)
   :config
   (flycheck-clang-tidy-setup))
+
 
 ;; TODO: setup a dir for compile-commands (read from file-specific conf file for the project)
 ;; TODO: my/get-clangd-compile-commands-die
@@ -31,29 +44,37 @@
   :hook (python-mode . (lambda ()
                          (require 'lsp-pyright)
                          (lsp)))  ;; or (lsp-deferred)
-  :config
-  (setq lsp-pyright-venv-path "~/src/pyenv"
-        lsp-pyright-python-executable-cmd "~/src/pyenv/global/bin/python3"
-        lsp-pyright-use-library-code-for-types t
-        lsp-pyright-typechecking-mode "workspace"))
+  :custom
+  (lsp-pyright-venv-path "~/src/pyenv")
+  (lsp-pyright-python-executable-cmd "~/src/pyenv/global/bin/python3")
+  (lsp-pyright-use-library-code-for-types t)
+  (lsp-pyright-typechecking-mode "workspace"))
 
+;; Formatting with Apheleia
 (use-package apheleia
-  :ensure t
+  :commands (apheleia-global-mode)
+  :init
+  ;; Forward-declare to silence “free variable” warnings for alists
+  (defvar apheleia-mode-alist)
+  (defvar apheleia-formatters)
   :config
-  (setf (alist-get 'python-mode apheleia-mode-alist)
-        '(isort black))
-
+  ;; mutate the alists after apheleia is loaded
+  (setf (alist-get 'python-mode apheleia-mode-alist) '(isort black))
   (setf (alist-get 'black apheleia-formatters)
         '("~/.config/emacs/formatters/black-wrapper"))
   (setf (alist-get 'isort apheleia-formatters)
         '("~/.config/emacs/formatters/isort-wrapper"))
-
   (apheleia-global-mode +1))
 
 
+;; Java (JDT LS)
 (use-package lsp-java
-  :ensure t
-  :config
-  (setq lsp-java-java-path "/usr/lib/jvm/java-23-openjdk/bin/java"
-        lsp-java-jdt-download-url "https://download.eclipse.org/jdtls/milestones/1.11.0/jdt-language-server-latest.tar.gz")
-  (add-hook 'java-mode-hook #'lsp))
+  :after lsp-mode
+  :hook (java-mode . lsp)
+  :custom
+  (lsp-java-java-path "/usr/lib/jvm/java-23-openjdk/bin/java")
+  (lsp-java-jdt-download-url
+   "https://download.eclipse.org/jdtls/milestones/1.11.0/jdt-language-server-latest.tar.gz"))
+
+(provide 'lsp-servers)
+;;; lsp-servers.el ends here

783223c12b333b2f2c40ac6d45591c84efe8b91f
Alexander Vasilyev <natsukashii_nane@proton.me>
2025-09-05 12:28:15 +0300

    new hotkeys

--- apps/gptel-setup.el
+++ apps/gptel-setup.el
@@ -3,3 +3,6 @@
   (setq gptel-default-mode 'org-mode)
   (setq gptel-api-key (my/select-and-set-openai-key))
   )
+
+(gptel-make-anthropic :name "CLAUDE" :stream t :key
+                      '"***REMOVED***")
